7b87f1cc5391f90012e9b093132e6240aa9ef797
E-Martynenko1982 <motor8103@gmail.com>
2025-01-04 00:49:45 +0200

    update

--- src/services/axiosClient.ts
+++ src/services/axiosClient.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 console.log("API Base URL:", import.meta.env.VITE_API_URL);
 
 const axiosClient = axios.create({
-	baseURL: import.meta.env.VITE_API_URL,
+	baseURL: "/api",
 	withCredentials: true,
 });
 
