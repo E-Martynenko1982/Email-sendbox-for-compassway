4612febe6d46bcea7bf65cb4a49b04e2fd6b3700
E-Martynenko1982 <motor8103@gmail.com>
2025-01-03 22:37:36 +0200

    update

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
 	server: {
 		proxy: {
 			"/api": {
-				target: "http://68.183.74.14:4005",
+				target: process.env.VITE_API_URL || "http://localhost:4005",
 				changeOrigin: true,
 				rewrite: (path) => path.replace(/^\/api/, "/api"),
 			},
